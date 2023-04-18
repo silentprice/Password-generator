71be255d289aa6b59b477db0dd45b207e0da21ad
Nick <nprice0502@yahoo.com>
2023-04-18 18:33:52 -0400

    created prompt for pw length and validated it

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Password-generator
+
+## Description
+* I updated the code given to me to generate a random password. 
+
+## Github repo url
+* 
+
+## Live URL
+* 
+
+## Screenshot
+![screenshot]
--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(length) {
-  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
-  var password = "";
-  for (var i = 0; i < length; i++) {
-    var randomIndex = Math.floor(math.random() * charset.length);
-    password += charset[randomIndex];
+// function generatePassword() {
+//   var numberOfCharacters = prompt("How many characters in the password?");
+//   var includesSpecialCharacters = confirm("Do you want special characters?");
+
+//   var charset =
+//     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+//   var password = "";
+//   for (var i = 0; i < length; i++) {
+//     var randomIndex = Math.floor(math.random() * charset.length);
+//     password += charset[randomIndex];
+//   }
+//   return password;
+// }
+
+function generatePassword() {
+  var pw = ""
+
+  var numberOfCharacters = prompt("How many characters in the password?");
+  while(numberOfCharacters < 8 || numberOfCharacters > 128 || isNaN(numberOfCharacters)) {
+    alert("Your password must be a number between 8 and 128 characters long")
+    numberOfCharacters = prompt("How many characters in the password?");
   }
-  return password;
+
+  return pw
 }
 
 // Write password to the #password input
-function writePassword(length) {
-  var password = generatePassword(8);
+function writePassword() {
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// teach tips...
