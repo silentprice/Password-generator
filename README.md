9f21092da9c8b975c83c368dbafa4b3c04804b99
Nick <nprice0502@yahoo.com>
2023-04-18 19:38:03 -0400

    added some comments and updated README screenshot"

--- README.md
+++ README.md
@@ -10,4 +10,4 @@
 * 
 
 ## Screenshot
-![screenshot]
+![screenshot](./images/ScreenshotFirstJS.png)
--- script.js
+++ script.js
@@ -14,16 +14,17 @@ var generateBtn = document.querySelector("#generate");
 //   }
 //   return password;
 // }
-// broke down the characters individually incase user does not want one applied
+
+// broke down the characters individually incase user does not want one applied to password
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
 var specials = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
-
+// create a function that  generates a password based on the users answers to the questions
 function generatePassword() {
   var pw = "";
   var tempChars = "";
-
+// afer user clicks generate password the first prompt will ask how many character in the password if they select a number lower than 8 or higher than 128 it will re ask the question
   var numberOfCharacters = prompt("How many characters in the password?");
   while (
     numberOfCharacters < 8 ||
@@ -33,31 +34,31 @@ function generatePassword() {
     alert("Your password must be a number between 8 and 128 characters long");
     numberOfCharacters = prompt("How many characters in the password?");
   }
-
+// next prompt will ask if user would like to use uppercase characters
   var wantsUpper = confirm("Would you like uppercase?");
   if (wantsUpper) {
     tempChars += uppercase;
   }
   console.log(tempChars);
-
+// the third prompt will ask if user would like lowercase characters
   var wantsLower = confirm("Would you like lowercase?");
   if (wantsLower) {
     tempChars += lowercase;
   }
   console.log(tempChars);
-
+// the fourth prompt will ask user if they would like to use numbers
   var wantsNumbers = confirm("Would you like numbers?");
   if (wantsNumbers) {
     tempChars += numbers;
   }
   console.log(tempChars);
-
+// the last prompt shall ask if they want to use special characters 
   var wantsSpecial = confirm("Would you like special characters?");
   if (wantsSpecial) {
     tempChars += specials;
   }
   console.log(tempChars);
-
+// create while loop so user must answer atleast one of the following prompts so password can be generated
   while (!wantsUpper && !wantsLower && !wantsNumbers && !wantsSpecial) {
     alert("You must choose at least one type of character");
     wantsUpper = confirm("Would you like uppercase?");
